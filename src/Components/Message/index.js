9d53f4e5d8539ddd13620148124aa5480d2d50da
PaulinaLL <paulinalll90@wp.pl>
2020-05-10 23:15:00 +0200

    Add setInterval

--- src/Components/Message/index.js
+++ src/Components/Message/index.js
@@ -3,15 +3,37 @@ import React from "react";
 class Message extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      index: 0,
+    };
   }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        index:
+          this.state.index === this.props.words.length - 1
+            ? 0
+            : this.state.index + 1,
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div className="cover">
-        <button onClick={this.props.showForm}>X</button>
-        <h2>
-          I am a <span>{this.props.words}</span>
-        </h2>
+        <div className="coverContentWrapper">
+          <button className="backToForm" onClick={this.props.showForm}>
+            X
+          </button>
+          <h2>
+            I am a <span>{this.props.words[this.state.index]}</span>
+          </h2>
+        </div>
       </div>
     );
   }
