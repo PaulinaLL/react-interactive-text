078cf0bf27b62280342ac5f2f9e9639b1236cd0e
PaulinaLL <paulinalll90@wp.pl>
2020-05-10 14:53:21 +0200

    Create App.js - displays Form or Message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import "./App.css";
+import Message from "./Components/Message";
+import Form from "./Components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <main className="App-main"></main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      words: [],
+      showResult: false,
+    };
+
+    this.parentTrigger = this.parentTrigger.bind(this);
+  }
+
+  parentTrigger(wordsArray) {
+    this.setState({
+      words: wordsArray,
+      showResult: wordsArray.length >= 3 ? true : false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main className="App-main">
+          {this.state.showResult ? (
+            <Message words={this.state.words} />
+          ) : (
+            <Form parentTrigger={this.parentTrigger} />
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
